5f08b5279438ed5d703e8085860edb700e53117e
niteshpawar97 <niteshpawar97@gmail.com>
2024-02-15 00:35:55 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,193 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MenuItemCard from "./components/MenuItemCard";
 
-function App() {
+const App = () => {
+  const menuItems = [
+    { id: 1, name: "Samosa", description: "Delicious samosa", price: 2.5 },
+    { id: 2, name: "Kachodi", description: "Tasty kachodi", price: 3 },
+    { id: 3, name: "Chai", description: "Refreshing chai", price: 1.5 },
+    { id: 4, name: "Biryani", description: "Flavorful biryani", price: 8 },
+    { id: 5, name: "Pav Bhaji", description: "Spicy pav bhaji", price: 6 },
+    {
+      id: 6,
+      name: "Paneer Tikka",
+      description: "Grilled paneer tikka",
+      price: 7,
+    },
+    { id: 7, name: "Dosa", description: "Crispy dosa with chutney", price: 4 },
+    { id: 8, name: "Pakora", description: "Crunchy pakora", price: 3.5 },
+    { id: 9, name: "Pizza", description: "Cheesy pizza", price: 9 },
+    { id: 10, name: "Burger", description: "Juicy burger", price: 5.5 },
+  ];
+
+  const [selectedItems, setSelectedItems] = useState([]);
+  const currentDate = new Date().toLocaleDateString();
+  const currentTime = new Date().toLocaleTimeString();
+  const storeName = "Garba Sweets";
+  const address =
+    "Stadium complex, Opp. BSNL Exchange, Parasia Rd, Chhindwara, M P 480001";
+  const contactNumber = "123-456-7890";
+  const invoiceNumber = Math.floor(Math.random() * 1000000); // Generate a random invoice number
+
+  const addToInvoice = (item) => {
+    const existingItem = selectedItems.find(
+      (selectedItem) => selectedItem.item.id === item.id
+    );
+    if (existingItem) {
+      existingItem.quantity++;
+      setSelectedItems([...selectedItems]);
+    } else {
+      setSelectedItems([...selectedItems, { item, quantity: 1 }]);
+    }
+  };
+
+  const removeFromInvoice = (itemId) => {
+    const updatedItems = selectedItems
+      .map((selectedItem) => {
+        if (selectedItem.item.id === itemId) {
+          selectedItem.quantity--;
+        }
+        return selectedItem;
+      })
+      .filter((selectedItem) => selectedItem.quantity > 0);
+    setSelectedItems(updatedItems);
+  };
+
+  const calculateSubtotal = () => {
+    return selectedItems.reduce(
+      (subtotal, selectedItem) =>
+        subtotal + selectedItem.item.price * selectedItem.quantity,
+      0
+    );
+  };
+
+  const calculateTotal = () => {
+    const subtotal = calculateSubtotal();
+    const taxRate = 0.1; // 10% tax rate
+    const taxAmount = subtotal * taxRate;
+    return (subtotal + taxAmount).toFixed(2);
+  };
+
+  const printInvoice = () => {
+    const printableContent = selectedItems
+      .map((selectedItem, index) => {
+        const totalItemPrice = (selectedItem.item.price * selectedItem.quantity).toFixed(2);
+        return `
+        <div style="margin-bottom: 10px;">
+          <p style="margin-bottom: 5px;">#${index + 1}# ${selectedItem.item.name} ${selectedItem.item.price.toFixed(2)} x (${selectedItem.quantity})  ₹ ${totalItemPrice}</p>
+          
+        </div>
+      `;
+      })
+      .join("");
+
+    const printableStyles = `
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          font-size: 12px;
+          line-height: 1.5;
+        }
+        p {
+          margin: 0;
+        }
+      </style>
+    `;
+
+    const printWindow = window.open("", "_blank");
+    printWindow.document.write(`
+      <html>
+        <head>
+          <title>Invoice</title>
+          ${printableStyles}
+        </head>
+        <body>
+          <div style="width: 3in; margin: 0 auto; padding: 10px;">
+            <h1 style="text-align: center; margin-bottom: 10px;">Invoice</h1>
+            <div>Date: ${currentDate}</div>
+            <div>Time: ${currentTime}</div>
+            <div>Store Name: ${storeName}</div>
+            <div>Address: ${address}</div>
+            <div>Contact Number: ${contactNumber}</div>
+            <div>Invoice Number: ${invoiceNumber}</div>
+            <hr style="margin: 10px 0;">
+            <div>#------Item---------Rate---QTY---Total</div>
+            ${printableContent}
+            <hr style="margin: 0px 0;">
+            <div style="margin-top: 10px;">
+              <p>Subtotal: ₹ ${calculateSubtotal()}</p>
+              <p>Tax (10%): ₹ ${(calculateSubtotal() * 0.1).toFixed(2)}</p>
+              <p>Total Amount: ₹ ${calculateTotal()}</p>
+            </div>
+          </div>
+        </body>
+      </html>
+    `);
+    printWindow.document.close();
+    printWindow.print();
+
+    // Close the print window after printing
+    printWindow.close();
+
+    // Reset selected items after printing
+    setSelectedItems([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <div className="bg-gray-100 p-4">
+        <h2 className="text-lg font-semibold mb-4">Store Name: {storeName}</h2>
+        <h2 className="text-lg font-semibold mb-4">Menu Items</h2>
+        <div className="flex flex-wrap justify-center">
+          {menuItems.map((item) => (
+            <div key={item.id} className="m-4">
+              <MenuItemCard item={item} />
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+                onClick={() => addToInvoice(item)}
+              >
+                Add to Invoice
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-1/2 bg-gray-200 p-4">
+        <h2 className="text-lg font-semibold mb-4">Selected Items</h2>
+        {selectedItems.map((selectedItem) => (
+          <div
+            key={selectedItem.item.id}
+            className="mb-2 p-2 bg-white rounded-lg flex justify-between items-center shadow"
+          >
+            <p>
+              {selectedItem.item.name}{" "}
+              {selectedItem.quantity > 1 && `(${selectedItem.quantity})`}
+            </p>
+            <p> ₹{selectedItem.item.price.toFixed(2)} x {selectedItem.quantity} = ₹{(selectedItem.item.price * selectedItem.quantity).toFixed(2)}</p>
+            <button
+              className="text-red-500 font-semibold"
+              onClick={() => removeFromInvoice(selectedItem.item.id)}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+        {selectedItems.length > 0 && (
+          <div className="mt-4">
+            <p className="text-lg font-semibold">Subtotal: ₹{calculateSubtotal()}</p>
+            <p className="text-lg font-semibold">Tax (10%): ₹{(calculateSubtotal() * 0.1).toFixed(2)}</p>
+            <p className="text-lg font-semibold">Total Amount: ₹{calculateTotal()}</p>
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4"
+              onClick={printInvoice}
+            >
+              Print Invoice
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MenuItemCard.js
@@ -0,0 +1,21 @@
+// src/components/MenuItemCard.js
+import React from 'react';
+import samosa from '../images/SamosaAloo.png'
+const MenuItemCard = ({ item }) => {
+  return (
+    <div className="max-w-xs w-full bg-green-300 shadow-lg rounded-lg overflow-hidden m-4">
+      <img
+        className="w-full h-56 object-cover object-center"
+        src={samosa}
+        alt="Item"
+      />
+      <div className="p-4">
+        <h2 className="text-gray-800 text-lg font-semibold mb-2">{item.name}</h2>
+        <p className="text-gray-600 text-sm mb-2">{item.description}</p>
+        <p className="text-gray-700 font-bold text-3xl">₹ {item.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItemCard;
